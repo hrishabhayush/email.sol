accc38f406fc42fd1e58adf34578cd494e13284c
rachelturneyy <rachelt397@gmail.com>
2025-12-02 22:45:04 -0500

    heading

--- Zero/apps/mail/components/home/HomeContent.tsx
+++ Zero/apps/mail/components/home/HomeContent.tsx
@@ -86,8 +86,7 @@ export default function HomeContent() {
           className="text-center text-4xl font-medium md:text-6xl"
         >
           <Balancer className="mb-3 max-w-[1130px]">
-            The incentivized inbox for richer replies.
-            Spend only for successful conversions.
+            The incentivized inbox for richer replies
           </Balancer>
         </motion.h1>
         <motion.p
